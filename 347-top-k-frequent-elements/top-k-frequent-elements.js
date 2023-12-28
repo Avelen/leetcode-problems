f08daf0bfc0142f5c042ec174e7e998fbc7fe8fa
Aleksandr <45632120+Avelen@users.noreply.github.com>
2023-12-29 00:41:22 +0300

    Time: 115 ms (5.32%) | Memory: 45.3 MB (55.89%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -11,9 +11,9 @@ var topKFrequent = function (nums, k) {
         hash[nums[i]] = hash[nums[i]] + 1 || 1;
     }
 
-    for (const [key, value] of Object.entries(hash)) {
-        res.push([key, value]);
-    }
+    // for (const [key, value] of ) {
+    //     res.push([key, value]);
+    // }
 
-    return res.sort((a, b) => b[1] - a[1]).filter((_, index) => index < k).map(el => el[0]);
+    return Object.entries(hash).sort((a, b) => b[1] - a[1]).filter((_, index) => index < k).map(el => el[0]);
 };
