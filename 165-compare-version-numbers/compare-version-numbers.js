dc4d949632683fb37a17ddd6d43d6bd0132a4e9d
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-01-07 18:22:01 +0300

    Time: 56 ms (27.59%) | Memory: 44.1 MB (7.33%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    let res = 0;
+    let v1 = version1.split('.');
+    let v2 = version2.split('.');
+    let length = Math.max(v1.length, v2.length);
+
+    while (true) {
+        if (length == 0) return res;
+
+        let v1Num = Number(v1.shift()) || 0;
+        let v2Num = Number(v2.shift()) || 0;
+
+        console.log(v1Num, v2Num);
+
+        if (v1Num > v2Num) {
+            return res = 1;
+        } else if (v1Num < v2Num) {
+            return res = -1;
+        }
+        length--;
+    }
+};
