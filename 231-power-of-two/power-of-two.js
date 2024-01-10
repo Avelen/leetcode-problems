07c5fe88d0c84f9326248300324bf121990cae1a
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-01-11 02:36:27 +0300

    Time: 71 ms (23.30%) | Memory: 43.5 MB (65.07%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -4,10 +4,5 @@
  */
 var isPowerOfTwo = function(n, i = 0) {
     if (Math.pow(2, i) > n) return false;
-
-    if (Math.pow(2, i) == n) {
-        return true;
-    } else {
-        return isPowerOfTwo(n, i + 1);
-    }
+    return Math.pow(2, i) == n ? true : isPowerOfTwo(n, i + 1);
 };
