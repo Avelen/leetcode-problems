7988510621fbaa89014cb52027cd653da00c193c
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-01-23 23:33:31 +0100

    Time: 99 ms (15.38%) | Memory: 48.7 MB (5.13%) - LeetSync

---
+++ 1900-closest-dessert-cost/closest-dessert-cost.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} baseCosts
+ * @param {number[]} toppingCosts
+ * @param {number} target
+ * @return {number}
+ */
+var closestCost = function (baseCosts, toppingCosts, target) {
+    let desertCost = Infinity;
+    const toppingCount = toppingCosts.length;
+
+    const makeDesert = (basePrice, toppingIndex) => {
+        if (Math.abs(target - basePrice) < Math.abs(target - desertCost)) desertCost = basePrice;
+        if (Math.abs(target - basePrice) === Math.abs(target - desertCost) && basePrice < desertCost) {
+            desertCost = basePrice;
+        }
+
+        if (toppingIndex >= toppingCount) return;
+        const toppingCost = toppingCosts[toppingIndex];
+
+        makeDesert(basePrice, toppingIndex + 1);
+        makeDesert(basePrice + toppingCost, toppingIndex + 1);
+        makeDesert(basePrice + toppingCost * 2, toppingIndex + 1);
+    }
+
+    for (const basePrice of baseCosts) {
+        makeDesert(basePrice, 0);
+    }
+
+    return desertCost;
+};
