bc2e5ce9ec023fd11a2605241289c9c9d9508ea6
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-02-01 00:06:59 +0100

    Time: 66 ms (86.21%) | Memory: 52.5 MB (19.86%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    let hash = {};
+
+    for (const char of s1) {
+        hash[char] = hash[char] + 1 || 1;
+    }
+
+    console.log(hash);
+
+    let left = 0;
+    let right = 0;
+    let count = s1.length;
+
+    while (right < s2.length) {
+        if (hash[s2[right]] > 0) count--;
+        hash[s2[right]]--;
+        right++;
+
+        if (count === 0) return true;
+
+        if (right - left === s1.length) {
+            if (hash[s2[left]] >= 0) count++;
+            hash[s2[left]]++;
+            left++;
+        }
+    }
+
+    return false;
+};
