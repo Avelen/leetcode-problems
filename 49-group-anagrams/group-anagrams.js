941c9af76bfed392110f7d266e850f68ca24828d
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-02-06 17:41:36 +0100

    Time: 123 ms (36.26%) | Memory: 62.4 MB (22.34%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,17 +3,22 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    // iterate the loop
+    // create the hash dictionary for each word when key be sorted word
     let hash = {};
+    let result = [];
 
     for (let i = 0; i < strs.length; i++) {
-        let curr = strs[i].split('').sort().join('');
+        let currStr = strs[i].split('')
+            .sort((a,b) => a.charCodeAt() - b.charCodeAt())
+            .join('');
 
-        if (hash[curr]) {
-            hash[curr].push(strs[i]);
+        if (hash[currStr] != undefined) {
+            result[hash[currStr]].push(strs[i]);
         } else {
-            hash[curr] = [strs[i]];
+            hash[currStr] = result.length;
+            result.push([strs[i]]);
         }
     }
-
-    return Object.values(hash);
+    return result;
 };
