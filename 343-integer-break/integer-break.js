f68717c76345dc8f42fba7706b8fa59201dfa93e
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-02-18 18:27:07 +0100

    Time: 52 ms (67.02%) | Memory: 48.8 MB (35.11%) - LeetSync

---
+++ 343-integer-break/integer-break.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+    if (n <= 3) return n - 1;
+
+    ans = 1;
+
+    while (n > 4) {
+        ans *= 3;
+        n -= 3;
+    }
+
+    return ans * n;
+};
