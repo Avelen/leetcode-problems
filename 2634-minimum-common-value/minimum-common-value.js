020e98fbf704356ff95e0538b69d55966f2771d7
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-03-15 22:39:58 +0100

    Time: 58 ms (90.81%) | Memory: 56 MB (94.31%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+    let p1 = 0;
+    let p2 = 0;
+
+    while (p1 < nums1.length && p2 < nums2.length) {
+        if (nums1[p1] < nums2[p2]) {
+            p1++
+        } else if (nums1[p1] > nums2[p2]) {
+            p2++
+        } else {
+            return nums1[p1];            
+        }
+    }
+
+    return -1;
+};
