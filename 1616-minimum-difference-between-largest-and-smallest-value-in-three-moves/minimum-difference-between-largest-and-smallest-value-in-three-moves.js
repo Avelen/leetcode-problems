c66e81bb866e65b2e2a83f770642a55c791ec9a1
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-04-18 12:56:36 +0200

    Time: 125 ms (33.33%) | Memory: 58.4 MB (51.28%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.js
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
 var minDifference = function(nums) {
-    if (nums.length < 5) return 0;
-    nums.sort((a, b) => a - b);
     let n = nums.length;
+
+    if (n < 5) return 0;
+
+    nums.sort((a, b) => a - b);
 
     return Math.min(
         nums[n - 4] - nums[0],
@@ -13,5 +15,4 @@ var minDifference = function(nums) {
         nums[n - 2] - nums[2],
         nums[n - 1] - nums[3]
     )
-
 };
