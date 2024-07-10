4a3b8dfa47c93dbdc59fd9cc580599ee4e1dd5ce
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-07-10 20:33:24 +0200

    Time: 48 ms (96.94%) | Memory: 51.7 MB (52.23%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let hash = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        hash[nums[i]] = hash[nums[i]] + 1 || 1;
+
+        const n = nums.length / 2;
+
+        if (hash[nums[i]] > n) {
+            return nums[i];
+        }
+    }
+};
