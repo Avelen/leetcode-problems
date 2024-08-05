f1c05929f252df4ef6b7f808cb090a29e2d61e49
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-08-05 20:30:41 +0300

    Time: 69 ms (5.03%) | Memory: 49.1 MB (33.65%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.js
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.js
@@ -7,14 +7,8 @@ var kthFactor = function(n, k) {
     let counter = 0;
 
     for (let i = 0; i <= n; i++) {
-
-        if (n % i === 0) {
-            counter++;
-        }
-
-        if (counter === k) {
-            return i;
-        }
+        if (n % i === 0) counter++;
+        if (counter === k) return i;
     }
 
     return -1;    
