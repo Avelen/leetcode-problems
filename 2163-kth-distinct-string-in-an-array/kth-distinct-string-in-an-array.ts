b3153b50083e098977774b7d462cf053dedfbbfd
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-08-05 20:09:53 +0300

    Time: 98 ms (14.29%) | Memory: 57.9 MB (7.14%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.ts
@@ -0,0 +1,17 @@
+function kthDistinct(arr: string[], k: number): string {
+    let hash = {}
+
+    for (let i = 0; i < arr.length; i++) {
+        hash[arr[i]] = hash[arr[i]] + 1 || 1;
+    }
+console.log(hash);
+    for (let h in hash) {
+        if (hash[h] === 1 && k === 1) return h;
+
+        if (hash[h] === 1) {
+            k-= 1;
+        }
+    }
+
+    return '';
+};
