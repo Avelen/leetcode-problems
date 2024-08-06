eb81c2165db5cdc375cb0ee5bad777cb5cdd96c5
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-08-07 00:37:37 +0300

    Time: 194 ms (40.82%) | Memory: 66 MB (36.74%) - LeetSync

--- 2458-find-minimum-time-to-finish-all-jobs-ii/find-minimum-time-to-finish-all-jobs-ii.js
+++ 2458-find-minimum-time-to-finish-all-jobs-ii/find-minimum-time-to-finish-all-jobs-ii.js
@@ -6,10 +6,7 @@
 var minimumTime = function(jobs, workers) {
     const sortedJob = jobs.sort((a, b) => a - b);
     const sortedWorkers = workers.sort((a,b) => a - b);
-
-    const dayList = sortedWorkers.map((w, i) => {
-        return Math.ceil(sortedJob[i] / w) || 1;
-    });
+    const dayList = sortedWorkers.map((w, i) => Math.ceil(sortedJob[i] / w));
 
     return Math.max(...dayList);
 };
