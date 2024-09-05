6dbf66a1ac18ccc921bf84e138bf859fab7ef17a
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-09-05 23:00:40 +0200

    Time: 122 ms (63.58%) | Memory: 69.2 MB (70.09%) - LeetSync

---
+++ 2742-group-by/group-by.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    let ans = {};
+
+    for (const item of this) {
+        const key = fn(item);
+
+        if (ans[key]) {
+            ans[key].push(item);
+        } else {
+            ans[key] = [item];
+        }
+    }
+
+    return ans;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
