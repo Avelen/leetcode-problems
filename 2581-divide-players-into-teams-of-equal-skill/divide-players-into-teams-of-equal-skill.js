fc540c1a42e18658aa500d322722de8c58aae0e7
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-10-04 11:47:25 +0200

    Time: 85 ms (88.16%) | Memory: 60.2 MB (53.95%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.js
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.js
@@ -2,25 +2,49 @@
  * @param {number[]} skill
  * @return {number}
  */
+// without sorting:
 var dividePlayers = function (skill) {
-    const sortedSkills = skill.sort((a, b) => a - b);
-    let left = 0;
-    let right = sortedSkills.length - 1;
-    const teams = [];
-    let pow = sortedSkills[left] + sortedSkills[right];
+    let maxPow = Math.max(...skill);
+    let minPow = Math.min(...skill);
 
-    while (left < right) {
-        const currPow = sortedSkills[left] + sortedSkills[right];
+    let pow = maxPow + minPow; // 6
+    let teams = [];
+    let teamsToPair = {};
 
-        if (pow !== currPow) {
-            return -1;
+    for (let i = 0; i < skill.length; i++) { // 3
+        const curr = skill[i];
+        const rem = pow - curr; // 3
+
+        if (teamsToPair[rem]) {
+            teams.push(rem * curr);
+            teamsToPair[rem] -= 1;
+        } else {
+            teamsToPair[curr] = teamsToPair[curr] + 1 || 1;
         }
-
-        teams.push(sortedSkills[left] * sortedSkills[right]);
-
-        left++;
-        right--;
     }
 
-    return teams.reduce((a, b) => a + b);
-};
+    return teams.length < Math.floor(skill.length / 2) ? -1 : teams.reduce((a, b) => a + b);
+}
+
+// var dividePlayers = function (skill) {
+//     const sortedSkills = skill.sort((a, b) => a - b);
+//     let left = 0;
+//     let right = sortedSkills.length - 1;
+//     const teams = [];
+//     let pow = sortedSkills[left] + sortedSkills[right];
+
+//     while (left < right) {
+//         const currPow = sortedSkills[left] + sortedSkills[right];
+
+//         if (pow !== currPow) {
+//             return -1;
+//         }
+
+//         teams.push(sortedSkills[left] * sortedSkills[right]);
+
+//         left++;
+//         right--;
+//     }
+
+//     return teams.reduce((a, b) => a + b);
+// };
