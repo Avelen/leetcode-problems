fdd9162f4cd30068390792b95fe8b5d7a9a01bcd
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-10-05 16:21:47 +0200

    Time: 386 ms (27.63%) | Memory: 96.4 MB (34.12%) - LeetSync

---
+++ 2858-join-two-arrays-by-id/join-two-arrays-by-id.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function (arr1, arr2) {
+    let combinedArr = [...arr1, ...arr2];
+    let result = {};
+
+    for (let el of combinedArr) {
+        const id = el.id;
+
+        if (!result[id]) {
+            result[id] = { ...el }
+        } else {
+            result[id] = { ...result[id], ...el }
+        }
+    }
+
+    return Object.values(result);
+};
