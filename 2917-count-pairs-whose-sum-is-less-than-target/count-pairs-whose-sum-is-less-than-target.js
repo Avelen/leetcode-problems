03fb36b98b9fbbcc4be34a38c092e38d7455fe42
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-12-16 23:24:00 +0100

    Time: 2 ms (37.81%) | Memory: 51.5 MB (45.74%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
@@ -5,14 +5,31 @@
  */
 var countPairs = function(nums, target) {
     let counter = 0;
+    
+    nums.sort((a, b) => a - b);
+    let left = 0;
+    let right = nums.length - 1;
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if ((nums[i] + nums[j]) < target) {
-                counter++;
-            }
+    while (left < right) {
+        if (nums[right] + nums[left] < target) {
+            counter += right - left;
+            left++;
+        } else {
+            right--;
         }
     }
-
+    
     return counter;
+    
+    // let counter = 0;
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         if ((nums[i] + nums[j]) < target) {
+    //             counter++;
+    //         }
+    //     }
+    // }
+
+    // return counter;
 };
