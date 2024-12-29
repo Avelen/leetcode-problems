d65daceeead6758e74226f23b956c75eb3df0935
Aleksandr <45632120+Avelen@users.noreply.github.com>
2024-12-29 11:09:05 +0100

    Time: 0 ms (100.00%) | Memory: 48.9 MB (84.10%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -11,10 +11,10 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-    if (!p && !q) return true;
+var isSameTree = function (p, q) {
     if (!p && q || p && !q) return false;
-    if (p === q) return true;
+    if (!p && !q) return true;
     if (p.val !== q.val) return false;
+
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
