97b9f341b00d12c2a88da3aa773a01eec5b5b0b1
Aleksandr <45632120+Avelen@users.noreply.github.com>
2025-01-15 22:24:42 +0100

    Time: 3 ms (59.44%) | Memory: 62.4 MB (49.97%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.js
@@ -4,12 +4,10 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    let max = -Infinity;
-
     if (nums.length === 1) return nums[0];
 
     let cur = nums.slice(0, k).reduce((a, b) => a + b);
-    max = Math.max(max, cur / k);
+    let max = cur / k;
 
     for (let i = 0; i < nums.length - k; i++) {
         cur = cur - nums[i] + nums[i + k];
